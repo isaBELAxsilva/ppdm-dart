24d53cede8d8c1a6abf5489eefbe08a344defc8f
isaBELAxsilva <133895409+isaBELAxsilva@users.noreply.github.com>
2024-03-04 10:26:08 -0300

    Atv1.dart

--- A3/Atv1.dart
+++ A3/Atv1.dart
@@ -1,5 +1,5 @@
 void main() {
-  List numeros = [10.0, 8.0, 9.0, 7.0, 10.0, 7.5];
+  List numeros = [10.0, 8.0, 9.0, 7.0 , 10.0, 7.5, 9.0, 9.4];
 
   double soma = 0.0;
   for (double numero in numeros) {
